5accbf29bf7a21b714c7b0603f9f7b62c7d53e81
mhkyg <spamfioka@mail.ru>
2017-04-28 19:41:32 +0200

    Libs

---
+++ DHT22/dht22_example.py
@@ -0,0 +1,28 @@
+from pyA20.gpio import gpio
+from pyA20.gpio import port
+
+#import RPi.GPIO as GPIO
+import dht22
+import time
+import datetime
+
+# initialize GPIO
+#gpio.setwarnings(False)
+#gpio.setmode(GPIO.BCM)
+PIN2 = 15
+gpio.init()
+#gpio.cleanup()
+
+
+# read data using pin 14
+instance = dht22.DHT22(pin=PIN2)
+
+while True:
+    result = instance.read()
+    if result.is_valid():
+        print("Last valid input: " + str(datetime.datetime.now()))
+        print("Temperature: %d C" % result.temperature)
+        print("Humidity: %d %%" % result.humidity)
+    else:
+        print("Error Code: %d" % result.error_code)
+    time.sleep(5)
---
+++ lcd/char_lcd_test.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+# Example using a character LCD connected to a Raspberry Pi or BeagleBone Black.
+import time
+
+from char_lcd2 import OrangePiZero_CharLCD as LCD
+
+
+# Raspberry Pi pin configuration:
+lcd_rs        = 14  # Note this might need to be changed to 21 for older revision Pi's.
+lcd_en        = 10
+lcd_d4        = 12
+lcd_d5        = 11
+lcd_d6        = 6
+lcd_d7        = 0
+lcd_backlight = 13
+
+# BeagleBone Black configuration:
+# lcd_rs        = 'P8_8'
+# lcd_en        = 'P8_10'
+# lcd_d4        = 'P8_18'
+# lcd_d5        = 'P8_16'
+# lcd_d6        = 'P8_14'
+# lcd_d7        = 'P8_12'
+# lcd_backlight = 'P8_7'
+
+# Define LCD column and row size for 16x2 LCD.
+lcd_columns = 16
+lcd_rows    = 2
+
+# Alternatively specify a 20x4 LCD.
+# lcd_columns = 20
+# lcd_rows    = 4
+
+# Initialize the LCD using the pins above.
+lcd = LCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
+                           lcd_columns, lcd_rows, lcd_backlight)
+
+# Print a two line message
+lcd.message('Hello\nworld!')
+
+# Wait 5 seconds
+time.sleep(5.0)
+
+# Demo showing the cursor.
+lcd.clear()
+lcd.show_cursor(True)
+lcd.message('Show cursor')
+
+time.sleep(5.0)
+
+# Demo showing the blinking cursor.
+lcd.clear()
+lcd.blink(True)
+lcd.message('Blink cursor')
+
+time.sleep(5.0)
+
+# Stop blinking and showing cursor.
+lcd.show_cursor(False)
+lcd.blink(False)
+
+# Demo scrolling message right/left.
+lcd.clear()
+message = 'Scroll'
+lcd.message(message)
+for i in range(lcd_columns-len(message)):
+    time.sleep(0.5)
+    lcd.move_right()
+for i in range(lcd_columns-len(message)):
+    time.sleep(0.5)
+    lcd.move_left()
+
+# Demo turning backlight off and on.
+lcd.clear()
+lcd.message('Flash backlight\nin 5 seconds...')
+time.sleep(5.0)
+# Turn backlight off.
+lcd.set_backlight(0)
+time.sleep(2.0)
+# Change message.
+lcd.clear()
+lcd.message('Goodbye!')
+# Turn backlight on.
+lcd.set_backlight(1)
